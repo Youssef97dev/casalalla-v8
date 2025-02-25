f2948dd9e0e397d86bd69d184f4c0a7bbb4bcf12
Youssef97dev <youssefmoussafi97@gmail.com>
2025-02-25 02:08:53 +0000

    add analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Casa Lalla Takerkoust",
@@ -9,6 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
+      <Analytics />
     </html>
   );
 }
