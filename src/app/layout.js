6a32d51e50d2fac52b374866783128e41ac71304
Youssef97dev <youssefmoussafi97@gmail.com>
2025-03-06 13:52:08 +0000

    change favicon 4

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,12 @@
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 
+export const metadata = {
+  title: "Casa Lalla Takerkoust",
+  description: "Exquisite cuisine and amazing panoramic view",
+  favicon: "/favicon.ico",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
